f220aac606330866396fa7f7710d90c74fab6ce9
chanjun.park <chanjun.park@navercorp.com>
2021-11-21 15:06:59 +0900

    state 코드랩 진행

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoScreen.kt
@@ -17,21 +17,17 @@
 package com.codelabs.state.todo
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelabs.state.util.generateRandomTodoItem
@@ -46,34 +42,66 @@ import kotlin.random.Random
  */
 @Composable
 fun TodoScreen(
-    items: List<TodoItem>,
-    onAddItem: (TodoItem) -> Unit,
-    onRemoveItem: (TodoItem) -> Unit
+	items: List<TodoItem>,
+	currentlyEditing: TodoItem?,
+	onAddItem: (TodoItem) -> Unit,
+	onRemoveItem: (TodoItem) -> Unit,
+	onStartEdit: (TodoItem) -> Unit,
+	onEditItemChange: (TodoItem) -> Unit,
+	onEditDone: () -> Unit
 ) {
-    Column {
-        LazyColumn(
-            modifier = Modifier.weight(1f),
-            contentPadding = PaddingValues(top = 8.dp)
-        ) {
-            items(items = items) {
-                TodoRow(
-                    todo = it,
-                    onItemClicked = { onRemoveItem(it) },
-                    modifier = Modifier.fillParentMaxWidth()
-                )
-            }
-        }
+	Column {
+		val enableTopSection = currentlyEditing == null
+		TodoItemInputBackground(elevate = enableTopSection, modifier = Modifier.fillMaxWidth()) {
+			if (enableTopSection) {
+				TodoItemEntryInput(onItemComplete = onAddItem)
+			} else {
+				Text(
+					"Editing item",
+					style = MaterialTheme.typography.h6,
+					textAlign = TextAlign.Center,
+					modifier = Modifier
+						.align(Alignment.CenterVertically)
+						.padding(16.dp)
+						.fillMaxWidth()
+				)
+			}
+		}
 
-        // For quick testing, a random item generator button
-        Button(
-            onClick = { onAddItem(generateRandomTodoItem()) },
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-        ) {
-            Text("Add random item")
-        }
-    }
+		LazyColumn(
+			modifier = Modifier.weight(1f),
+			contentPadding = PaddingValues(top = 8.dp)
+		) {
+			items(items = items) { todo ->
+
+				if (currentlyEditing?.id == todo.id) {
+					TodoItemInlineEditor(
+						item = todo,
+						onEditItemChange = onEditItemChange,
+						onEditDone = onEditDone,
+						onRemoveItem = { onRemoveItem(todo) }
+					)
+				} else {
+					TodoRow(
+						todo = todo,
+						onItemClicked = { onStartEdit(todo) },
+						modifier = Modifier.fillParentMaxWidth()
+					)
+				}
+
+			}
+		}
+
+		// For quick testing, a random item generator button
+		Button(
+			onClick = { onAddItem(generateRandomTodoItem()) },
+			modifier = Modifier
+				.padding(16.dp)
+				.fillMaxWidth(),
+		) {
+			Text("Add random item")
+		}
+	}
 }
 
 /**
@@ -84,40 +112,153 @@ fun TodoScreen(
  * @param modifier modifier for this element
  */
 @Composable
-fun TodoRow(todo: TodoItem, onItemClicked: (TodoItem) -> Unit, modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier
-            .clickable { onItemClicked(todo) }
-            .padding(horizontal = 16.dp, vertical = 8.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Text(todo.task)
-        Icon(
-            imageVector = todo.icon.imageVector,
-            contentDescription = stringResource(id = todo.icon.contentDescription)
-        )
-    }
+fun TodoRow(
+	todo: TodoItem,
+	onItemClicked: (TodoItem) -> Unit,
+	modifier: Modifier = Modifier,
+	iconAlpha: Float = remember(todo.id) { randomTint() }
+) {
+	Row(
+		modifier = modifier
+			.clickable { onItemClicked(todo) }
+			.padding(horizontal = 16.dp, vertical = 8.dp),
+		horizontalArrangement = Arrangement.SpaceBetween
+	) {
+		Text(todo.task)
+		Icon(
+			imageVector = todo.icon.imageVector,
+			contentDescription = stringResource(id = todo.icon.contentDescription),
+			tint = LocalContentColor.current.copy(alpha = iconAlpha)
+		)
+	}
 }
 
 private fun randomTint(): Float {
-    return Random.nextFloat().coerceIn(0.3f, 0.9f)
+	return Random.nextFloat().coerceIn(0.3f, 0.9f)
+}
+
+@Composable
+fun TodoItemInlineEditor(item: TodoItem,
+						 onEditItemChange: (TodoItem) -> Unit,
+						 onEditDone: () -> Unit,
+						 onRemoveItem: () -> Unit
+) = TodoItemInput(
+	text = item.task,
+	onTextChange = { onEditItemChange(item.copy(task = it)) },
+	icon = item.icon,
+	onIconChange = { onEditItemChange(item.copy(icon = it)) },
+	submit = onEditDone,
+	iconsVisible = true,
+	buttonSlot = {
+		Row {
+			val shrinkButtons = Modifier.widthIn(20.dp)
+			TextButton(onClick = onEditDone, modifier = shrinkButtons) {
+				Text(
+					text = "\uD83D\uDCBE", // floppy disk
+					textAlign = TextAlign.End,
+					modifier = Modifier.width(30.dp)
+				)
+			}
+			TextButton(onClick = onRemoveItem, modifier = shrinkButtons) {
+				Text(
+					text = "❌",
+					textAlign = TextAlign.End,
+					modifier = Modifier.width(30.dp)
+				)
+			}
+		}
+	}
+)
+
+@Composable
+fun TodoItemEntryInput(onItemComplete: (TodoItem) -> Unit) {
+	val (text, setText) = remember { mutableStateOf("") }
+	val (icon, setIcon) = remember { mutableStateOf(TodoIcon.Default) }
+	val iconVisible = text.isNotBlank()
+	val submit = {
+		onItemComplete(TodoItem(text, icon))
+		setIcon(TodoIcon.Default)
+		setText("")
+	}
+	TodoItemInput(
+		text = text,
+		onTextChange = setText,
+		icon = icon,
+		onIconChange = setIcon,
+		submit = submit,
+		iconsVisible = iconVisible
+	) {
+		TodoEditButton(onClick = submit, text = "Add", enabled = text.isNotBlank())
+	}
+}
+
+@Composable
+fun TodoItemInput(
+	text: String,
+	onTextChange: (String) -> Unit,
+	icon: TodoIcon,
+	onIconChange: (TodoIcon) -> Unit,
+	submit: () -> Unit,
+	iconsVisible: Boolean,
+	buttonSlot: @Composable () -> Unit
+) {
+	Column {
+		Row(
+			Modifier
+				.padding(horizontal = 16.dp)
+				.padding(top = 16.dp)
+		) {
+			TodoInputTextField(
+				modifier = Modifier
+					.weight(1f)
+					.padding(end = 8.dp),
+				text = text,
+				onTextChange = onTextChange,
+				onImeAction = submit
+			)
+
+			Spacer(modifier = Modifier.width(8.dp))
+			Box(Modifier.align(Alignment.CenterVertically)) {
+				buttonSlot()
+			}
+		}
+		if (iconsVisible) {
+			AnimatedIconRow(icon = icon, onIconChange = onIconChange, Modifier.padding(top = 8.dp))
+		} else {
+			Spacer(modifier = Modifier.height(18.dp))
+		}
+	}
+}
+
+@Composable
+fun TodoInputTextField(
+	modifier: Modifier,
+	text: String,
+	onTextChange: (String) -> Unit,
+	onImeAction: () -> Unit
+) {
+	TodoInputText(text, onTextChange, modifier, onImeAction)
 }
 
 @Preview
 @Composable
 fun PreviewTodoScreen() {
-    val items = listOf(
-        TodoItem("Learn compose", TodoIcon.Event),
-        TodoItem("Take the codelab"),
-        TodoItem("Apply state", TodoIcon.Done),
-        TodoItem("Build dynamic UIs", TodoIcon.Square)
-    )
-    TodoScreen(items, {}, {})
+	val items = listOf(
+		TodoItem("Learn compose", TodoIcon.Event),
+		TodoItem("Take the codelab"),
+		TodoItem("Apply state", TodoIcon.Done),
+		TodoItem("Build dynamic UIs", TodoIcon.Square)
+	)
+	TodoScreen(items, null, {}, {}, {}, {}, {})
 }
 
 @Preview
 @Composable
 fun PreviewTodoRow() {
-    val todo = remember { generateRandomTodoItem() }
-    TodoRow(todo = todo, onItemClicked = {}, modifier = Modifier.fillMaxWidth())
+	val todo = remember { generateRandomTodoItem() }
+	TodoRow(todo = todo, onItemClicked = {}, modifier = Modifier.fillMaxWidth())
 }
+
+@Preview
+@Composable
+fun PreviewTodoItemInput() = TodoItemEntryInput(onItemComplete = { })
--- week 2-2-Using state in Jetpack Compose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/test/java/com/codelabs/state/todo/TodoViewModelTest.kt
@@ -16,6 +16,186 @@
 
 package com.codelabs.state.todo
 
+import com.codelabs.state.util.generateRandomTodoItem
+import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import java.lang.IllegalArgumentException
+
 class TodoViewModelTest {
-    // TODO: Write tests
+    @Test
+	fun whenRemoveItem_updateList() {
+		val viewModel = TodoViewModel()
+		val item1 = generateRandomTodoItem()
+		val item2 = generateRandomTodoItem()
+		viewModel.addItem(item1)
+		viewModel.addItem(item2)
+
+		viewModel.removeItem(item1)
+
+		assertThat(viewModel.todoItems).isEqualTo(listOf(item2))
+	}
+
+	@Test
+	fun whenAddItem_updateList() {
+		val viewModel = TodoViewModel()
+
+		assertThat(viewModel.todoItems).isEqualTo(emptyList<TodoItem>())
+
+		val item1 = generateRandomTodoItem()
+		viewModel.addItem(item1)
+
+		assertThat(viewModel.todoItems).isEqualTo(listOf(item1))
+	}
+
+	@Test
+	fun onEditItemSelected_updateCurrentEditPosition() {
+		val viewModel = TodoViewModel()
+
+		val item1 = generateRandomTodoItem()
+		val item2 = generateRandomTodoItem()
+		val item3 = generateRandomTodoItem()
+
+		viewModel.addItem(item1)
+		viewModel.addItem(item2)
+		viewModel.addItem(item3)
+
+		assertTrue(viewModel.currentEditPosition == -1)
+
+		viewModel.onEditItemSelected(item2)
+
+		assertTrue(viewModel.currentEditPosition == viewModel.todoItems.indexOf(item2))
+	}
+
+	@Test
+	fun onEditDone_updateCurrentEditPosition() {
+		val viewModel = TodoViewModel()
+
+		val item1 = generateRandomTodoItem()
+		val item2 = generateRandomTodoItem()
+		val item3 = generateRandomTodoItem()
+
+		viewModel.addItem(item1)
+		viewModel.addItem(item2)
+		viewModel.addItem(item3)
+
+		viewModel.onEditItemSelected(item2)
+
+		viewModel.onEditDone()
+
+		assertTrue(viewModel.currentEditPosition == -1)
+	}
+
+	@Test
+	fun onEditItemChange_updateList() {
+		val viewModel = TodoViewModel()
+
+		val item1 = generateRandomTodoItem()
+		val item2 = generateRandomTodoItem()
+		val item3 = generateRandomTodoItem()
+
+		viewModel.addItem(item1)
+		viewModel.addItem(item2)
+		viewModel.addItem(item3)
+
+		viewModel.onEditItemSelected(item2)
+
+		val newItem2 = item2.copy(task = "Test Task")
+
+		viewModel.onEditItemChange(newItem2)
+
+		assertThat(viewModel.currentEditItem).isEqualTo(newItem2)
+	}
+
+	@Test
+	fun whenNotEditing_currentEditItemIsNull() {
+		val viewModel = TodoViewModel()
+
+		val item = generateRandomTodoItem()
+		viewModel.addItem(item)
+
+		assertThat(viewModel.currentEditItem).isNull()
+	}
+
+	@Test
+	fun whenEditing_currentEditItemIsCorrectItem() {
+		val viewModel = TodoViewModel()
+
+		val item1 = generateRandomTodoItem()
+		val item2 = generateRandomTodoItem()
+		val item3 = generateRandomTodoItem()
+
+		viewModel.addItem(item1)
+		viewModel.addItem(item2)
+		viewModel.addItem(item3)
+
+		viewModel.onEditItemSelected(item2)
+
+		assertThat(viewModel.currentEditItem).isEqualTo(item2)
+	}
+
+	@Test
+	fun whenEditingDone_currentEditItemIsCorrectItem() {
+		val viewModel = TodoViewModel()
+
+		val item1 = generateRandomTodoItem()
+		val item2 = generateRandomTodoItem()
+
+		viewModel.addItem(item1)
+		viewModel.addItem(item2)
+
+		viewModel.onEditItemSelected(item2)
+
+		viewModel.onEditDone()
+
+		assertThat(viewModel.currentEditItem).isNull()
+	}
+
+	@Test
+	fun whenEditingItem_updateAreShownInItemAndList() {
+		val viewModel = TodoViewModel()
+
+		val item1 = generateRandomTodoItem()
+		val item2 = generateRandomTodoItem()
+
+		viewModel.addItem(item1)
+		viewModel.addItem(item2)
+
+		viewModel.onEditItemSelected(item2)
+
+		val newItem2 = item2.copy(task = "Test Task")
+
+		viewModel.onEditItemChange(newItem2)
+
+		assertThat(viewModel.currentEditItem).isEqualTo(newItem2)
+		assertThat(viewModel.todoItems).isEqualTo(listOf(item1, newItem2))
+	}
+
+	@Test(expected = IllegalArgumentException::class)
+	fun whenEditing_wrongItemThrows() {
+		val viewModel = TodoViewModel()
+
+		val item1 = generateRandomTodoItem()
+		val item2 = generateRandomTodoItem()
+
+		viewModel.addItem(item1)
+		viewModel.addItem(item2)
+
+		viewModel.onEditItemSelected(item2)
+
+		val newItem1 = item1.copy(task = "Test Task")
+
+		viewModel.onEditItemChange(newItem1)
+	}
+
+	@Test(expected = IllegalArgumentException::class)
+	fun whenNoEditing_onEditItemChangeThrows() {
+		val viewModel = TodoViewModel()
+
+		val item1 = generateRandomTodoItem()
+
+		viewModel.addItem(item1)
+
+		viewModel.onEditItemChange(item1)
+	}
 }
